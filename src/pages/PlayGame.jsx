01a58d1c7d53591c9d1274a8c4abdca58b976db2
AnmolSharma-git <148962440+AnmolSharma-git@users.noreply.github.com>
2026-07-08 19:51:33 +0530

    added letter buttons and functionality

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,13 +1,28 @@
 import { Link, useLocation, useSearchParams } from "react-router-dom";
 import MaskedText from "../components/MaskedText/MaskedText";
+import LetterButttons from "../components/LetterButtons/LetterButtons";
+import { useState } from "react";
 function PlayGame(){
 
     const {state} = useLocation();
-    // console.log(searchParams.get("text"));
+
+    //we want our component to rerender when guessedLetters state changes. So we will use useState hook to create a state variable guessedLetters and a function setGuessedLetters to update it.
+    const [guessedLetters,setGuessedLetters]= useState([]);
+    function handleLetterClick(letter){
+        setGuessedLetters(previousLetters => {
+            if(previousLetters.includes(letter)){
+                return previousLetters;
+            }
+            return [...previousLetters,letter];
+        });
+    }
     return(
         <>
         <h1>Play Game</h1>
-        <MaskedText text={state.wordSelected} guessedLetters={[]}/>
+        <MaskedText text={state.wordSelected} guessedLetters={guessedLetters}/>
+        <div>
+            <LetterButttons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={handleLetterClick}/>
+        </div>
         <Link to='/start' className="text-blue-400">Start game link</Link>
         </>
     )
